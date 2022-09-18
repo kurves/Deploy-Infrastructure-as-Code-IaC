c966fc0ae21061bd4b59b46139edaa5fc6c7952e
kurves <kavssimply@gmail.com>
2022-09-18 23:16:25 +0300

    Create configure-aws-cli.sh
    configure aws cli

---
+++ configure-aws-cli.sh
@@ -0,0 +1,11 @@
+# Ensure that you have [AWS CLI tool](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html) installed.
+Verify using 
+```
+aws --version
+```
+Set u AWS CLI using
+```
+aws configure
+aws configure --profile default
+
+```
