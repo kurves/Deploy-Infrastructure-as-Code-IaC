b0a0ea1846c512f6875ac21b3b64988e8000a35b
kurves <kavssimply@gmail.com>
2022-09-19 23:13:16 +0300

    Update configure-aws-cli.sh

--- configure-aws-cli.sh
+++ configure-aws-cli.sh
@@ -8,11 +8,17 @@ Set u AWS CLI using
 aws configure
 aws configure --profile default
 
+#Create stack
 #aws command 
 aws cloudformation create-stack  --stack-name myFirstTest --region us-east-1 --template-body file://testcfn.yml
 
 #shell script command
 aws cloudformation create-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-2
+
+#Update stack
+
+aws cloudformation update-stack  --stack-name myFirstTest --region us-east-1 --template-body file://testcfn.yml
+
 
 
 
