f69010b5c0b7ec2e3c17bad41b39ddb87c15a317
kurves <kavssimply@gmail.com>
2022-10-07 00:37:25 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,4 @@
 #update-stack
+# change file permissions with
+# chmod +x update.sh
 aws cloudformation update-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
