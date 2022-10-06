db5aff61535e86755406e81881a4cccbcaa9d3a2
kurves <kavssimply@gmail.com>
2022-10-06 23:26:44 +0300

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,3 +1,6 @@
 
 # create-stack
+# run the shell scripts using
+# ./create.sh argumet_1 argument_2 argument_3
+
 aws cloudformation create-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
